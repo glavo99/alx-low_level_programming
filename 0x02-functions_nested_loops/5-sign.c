1f66f9a93587500e959ccfdab2322818bd5192dc
Yousef Elgendy <elgendy.yousef.1999@gmail.com>
2023-03-21 15:40:47 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_sign - checks for sign
+ * @n: number
+ * Return: integer
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+		_putchar('+');
+		return (1);
+	else if (n < 0)
+		_putchar('-');
+		return (-1);
+	else if (n == 0)
+		_putchar(48);
+		return (0);
+
+}
