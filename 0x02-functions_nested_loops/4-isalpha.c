9e4db5ceccd712d76ebcdfa50d3993ebc460e1f9
Yousef Elgendy <elgendy.yousef.1999@gmail.com>
2023-03-21 15:25:32 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for a letter
+ * @c: could be a letter or not
+ * Return: 1 if letter and 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if (i >= 65 && i <= 90)
+		return (1);
+	else if (i >= 97 && i <= 122)
+		return (1);
+	else
+		return (0);
+}
