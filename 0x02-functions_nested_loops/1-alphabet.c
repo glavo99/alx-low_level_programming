7952a0d040e945ca12b50ceecfa9c5c6ff529c05
Yousef Elgendy <elgendy.yousef.1999@gmail.com>
2023-03-21 14:31:51 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* print_alphabet - Print the alphabet, in lowercase, followed by a new line
+* Return: Always 0 (Success)
+*/
 void print_alphabet(void)
 {
 	char c;
-	for(c='a';c<='z';c++)
+
+	for (c = 'a'; c <= 'z'; c++)
 		_putchar(c);
 	_putchar('\n');
 }
