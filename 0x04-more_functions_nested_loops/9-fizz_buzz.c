b586a57ff296d0dd608891bdb70b543d1a5e5e89
Yousef Elgendy <elgendy.yousef.1999@gmail.com>
2023-03-23 15:39:33 -0700

    fizz_buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Fizz-Buzz test
+ * Return: void
+ */
+
+void main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf(" FizzBuzz");
+		}
+		else if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf(" Fizz");
+		}
+		else if (i % 3 != 0 && i % 5 == 0)
+		{
+			printf(" Buzz");
+		}
+		else if (i == 1)
+		{
+			printf("%d", i);
+		}
+		else
+		{
+			printf(" %d", i);
+		}
+	}
+}
