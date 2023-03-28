470a26d3dca7c7badd55403550bb5f794c3570ed
Yousef Elgendy <elgendy.yousef.1999@gmail.com>
2023-03-27 18:53:40 -0700

    print-array

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,11 +1,14 @@
 #ifndef main_H
 #define main_H
 
+#include <stdio.h>
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 int _putchar(char c);
 void print_rev(char *s);
+void print_array(int *a, int n);
 
 #endif
