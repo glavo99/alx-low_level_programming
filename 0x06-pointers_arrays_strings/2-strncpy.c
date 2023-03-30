d845fb9ba17be78bc9e6ac80d7be31aaf6ea97d7
Yousef Elgendy <elgendy.yousef.1999@gmail.com>
2023-03-29 18:49:53 -0700

    copy-str

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: first string
+ * @src: second string
+ * @n: number of bytes
+ * Return: char
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
