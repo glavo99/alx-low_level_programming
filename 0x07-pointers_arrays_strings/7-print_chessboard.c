5aca25063ebdef2f81a9ec811d4e4f19786ba569
Yousef Elgendy <elgendy.yousef.1999@gmail.com>
2023-04-03 18:29:52 -0700

    chess-board

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+# include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: array of pieces
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 1; i < 9; i++)
+	{
+		for (j = 1; j < 9; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
