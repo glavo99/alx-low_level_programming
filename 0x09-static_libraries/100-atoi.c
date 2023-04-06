d0939ba51c498dfe4803d796a86e1f41d194004d
Yousef Elgendy <elgendy.yousef.1999@gmail.com>
2023-04-06 03:27:55 -0700

    added-empty-files

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: character
+ * Return: integer
+ */
+
+int _atoi(char *s)
+{
+	return (1);
+}
---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: input value
+ * @needle: input value
+ * Return: char
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	return ('y');
+}
