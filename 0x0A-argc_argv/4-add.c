fee3593ab3d1f32ac267102f0d0d9360cbf8b737
Yousef Elgendy <elgendy.yousef.1999@gmail.com>
2023-04-10 11:47:05 -0700

    add numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: integer
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (!isdigit(argv[i])
+		{
+			printf("Error\n");	
+			return (1);	
+		}
+		else
+		{
+			j += atoi(argv[i]);
+		}
+	}
+	printf("%d\n", j);
+	return (0);
+}
