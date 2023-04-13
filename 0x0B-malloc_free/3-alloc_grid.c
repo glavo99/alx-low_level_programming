035f4f4aa6e9122e6bd20685b782e5418cd552b2
Yousef Elgendy <elgendy.yousef.1999@gmail.com>
2023-04-12 19:00:43 -0700

    alloc-grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -38,12 +38,12 @@ int **alloc_grid(int width, int height)
 				free(s);
 				return (NULL);
 			}
-		}
-	}
+		}	
 	for (i = 0; i < height; i++)
 	{
 		for (j = 0; j < width; j++)
 			s[i][j] = 0;
 	}
 	return (s);
+	}
 }
