47df554b7bc31334d7b64f328bda66580d8dbdbf
Yousef Elgendy <elgendy.yousef.1999@gmail.com>
2023-04-18 06:33:39 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,10 +16,8 @@ struct dog
 };
 
 /**
- * dog_t - new type
- * of structure dog
+ * dog_t - Typedef for dog
  */
-
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
