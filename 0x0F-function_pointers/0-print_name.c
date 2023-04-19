77cc069667ef10596570f12953ee97a85d04c91f
Yousef Elgendy <elgendy.yousef.1999@gmail.com>
2023-04-19 11:56:55 -0700

    print-name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,10 @@
 #include "function_pointers.h"
 
+/**
+ * print_name - prints a name
+ * @name: pointer to a name
+ * @f: pointer to a function
+*/
 void print_name(char *name, void (*f)(char *))
 {
 	if (!name || !f)
