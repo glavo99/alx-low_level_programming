b2184a71bd8f198edf91100550b727e9f06b7fda
Yousef Elgendy <elgendy.yousef.1999@gmail.com>
2023-04-20 07:44:12 -0700

    added main

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -27,7 +27,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(100);
 	}
-	if (get_op_func(s) == NULL)
+	if (get_op_func(s) == NULL || s[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
