dcd7fb3361f69b4293cc1bef7e6b608e78718e60
Yousef Elgendy <elgendy.yousef.1999@gmail.com>
2023-04-27 18:52:25 -0700

    list-len

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * list_len - returns the number of elements in a linked list_t list
+ * @h: pointer to first element
+ * Return: returns the number of elements in a linked list_t list
+ */
+size_t list_len(const list_t *h)
+{
+	if (h == NULL)
+	{
+		return (0);
+	}
+	return (1 + list_len(h->next));
+}
