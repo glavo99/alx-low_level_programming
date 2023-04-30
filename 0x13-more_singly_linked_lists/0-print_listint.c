200c576ec387abb3d66d54fcc1e34cde8d049033
Yousef Elgendy <elgendy.yousef.1999@gmail.com>
2023-04-30 11:51:02 -0700

    first task

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t list
+ * @h: pointer to first element
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	if (h == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		printf("%d\n", h->n)
+	return (1 + print_listint(h->next));
+}
