6c3ac396171f68b40d5dcf24ba7e1c3de990878f
Yousef Elgendy <elgendy.yousef.1999@gmail.com>
2023-05-09 15:12:58 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		i++;
 	}
-	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
+	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fp == -1)
 	{
 		return (-1);
