92a979a6241270ebd6fdea7d3b6bac4100f6ee9b
Yousef Elgendy <elgendy.yousef.1999@gmail.com>
2023-06-20 01:28:59 -0700

    basic operations

---
+++ 0x18-dynamic_libraries/operations/add.c
@@ -0,0 +1,10 @@
+/**
+ * add - add two numbers
+ * @a: first integer
+ * @b: second integer
+ * Return: answer
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x18-dynamic_libraries/operations/sub.c
@@ -0,0 +1,10 @@
+/**
+ * sub - subtract two numbers
+ * @a: first integer
+ * @b: second integer
+ * Return: answer
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
