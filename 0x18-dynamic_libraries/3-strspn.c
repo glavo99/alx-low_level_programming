3824daa5b919d3a1a2beb8b30b05a6192921cd67
Yousef Elgendy <elgendy.yousef.1999@gmail.com>
2023-06-20 00:54:01 -0700

    dynamic lib

---
+++ 0x18-dynamic_libraries/3-strspn.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string to be scaned
+ * @accept: string containing characters to match
+ * Return: number of bytes in the initial segment of s
+ * which consist only of bytes from accept
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != s[i]; j++)
+		{
+			if (accept[j + 1] == '\0')
+			{
+				return (i);
+			}
+		}
+	}
+	return (i);
+}
---
+++ 0x18-dynamic_libraries/4-strpbrk.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: string
+ * @accept: bytes
+ * Return: a pointer to the byte in s
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	return (s);
+}
